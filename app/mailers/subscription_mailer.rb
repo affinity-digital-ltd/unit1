98584721397fe80b935dfd160b2f6432c57c5516
krisquigley <kris@krisquigley.co.uk>
2017-12-30 18:04:03 +0000

    Added email for new posts

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -5,4 +5,10 @@ class SubscriptionMailer < ActionMailer::Base
     @email = email
     mail(to: @email, subject: "Welcome to Unit 1")
   end
+
+  def new_post_email(email, post_id)
+    @email = email
+    @post = Post.find(post_id)
+    mail(to: @email, subject: "A new post has been published!")
+  end
 end
