c864fd2c8d70fd9b58d5dfd77e078ea4c5e54af0
krisquigley <kris@krisquigley.co.uk>
2018-01-01 20:21:15 +0000

    Remove markdown helper

--- app/helpers/markdown_helper.rb
+++
@@ -1,6 +0,0 @@
-module MarkdownHelper
-  def render_markdown(text)
-    markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(escape_html: true), autolink: true, tables: true)
-    markdown.render(text)
-  end
-end
