2e9b8b592f33c83330cd19888c9ce6bb1e328046
Kris Quigley <kris@krisquigley.co.uk>
2018-03-09 16:06:52 +0000

    Added comment mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,4 +1,17 @@
 Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
+  field :addComment, Types::CommentType do
+    description "Adds a comment"
+    argument :postId, !types.Int
+    argument :name, !types.String
+    argument :body, !types.String
+
+    resolve ->(o,args,c) {
+      post = Post.find(args[:postId])
+      comment = post.comments.build(name: args[:name], body: args[:body])
+      comment.save!
+      comment
+    }
+  end
 end
